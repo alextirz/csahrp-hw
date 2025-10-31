bc64b67c35f780e1eaf16ceab67138239a7027b0
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-10-31 22:38:58 +0100

    Basic struct implementation

--- Homeworks/ConsoleStruct/DecimalNumber.cs
+++ Homeworks/ConsoleStruct/DecimalNumber.cs
@@ -2,6 +2,21 @@
 {
     internal struct DecimalNumber
     {
-        
+        public int Number { get; set; }
+
+        public string ToBinary()
+        {
+            return Convert.ToString(Number, 2);
+        }
+
+        public string ToOctal()
+        {
+            return Convert.ToString(Number, 8);
+        }
+
+        public string ToHex()
+        {
+            return Convert.ToString(Number, 16).ToUpper();
+        }
     }
 }
