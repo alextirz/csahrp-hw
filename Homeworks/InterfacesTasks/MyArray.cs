d70ab04f9c6e5e49e1ea0c60d0f2dda473b4b33d
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-05 18:50:08 +0100

    Add IOutput, its implementation and examples

---
+++ Homeworks/InterfacesTasks/MyArray.cs
@@ -0,0 +1,23 @@
+﻿namespace InterfacesTasks
+{
+    internal class MyArray : IOutput
+    {
+        private int[] numbers;
+
+        public MyArray(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Array elements: " + string.Join(", ", numbers));
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Console.WriteLine("Array elements: " + string.Join(", ", numbers));
+        }
+    }
+}
---
+++ Homeworks/InterfacesTasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using InterfacesTasks;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] data = { 5, 10, 15, 20 };
+        MyArray myArray = new MyArray(data);
+
+        myArray.Show();
+        Console.WriteLine();
+        myArray.Show("Here is the array with custom info:");
+    }
+}
