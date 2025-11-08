66cd36b58db8510da462fac6dde5bf4b3c8f49e1
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-08 10:43:05 +0100

    Add better descriptions to console

--- Homeworks/OperatorOverloading/Program.cs
+++ Homeworks/OperatorOverloading/Program.cs
@@ -1,39 +1,36 @@
-﻿using System;
-
-namespace OperatorOverloading
+﻿namespace OperatorOverloading
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Employee demo.");
             Employee employee1 = new Employee("Alex", 3000);
             Employee employee2 = new Employee("Mary", 3500);
 
             employee1.ShowInfo();
             employee2.ShowInfo();
 
+            Console.WriteLine();
+            Console.WriteLine("When salaries are equal.");
             employee1 += 500;
-
             employee1.ShowInfo();
             employee2.ShowInfo();
 
-           
-            Console.WriteLine("When salaries are equal.");
+            Console.WriteLine($"employee1 > employee2: {employee1 > employee2}");
+            Console.WriteLine($"employee1 < employee2: {employee1 < employee2}");
+            Console.WriteLine($"employee1 == employee2: {employee1 == employee2}");
+            Console.WriteLine($"employee1 != employee2: {employee1 != employee2}");
 
-            Console.WriteLine(employee1 > employee2); //false
-            Console.WriteLine(employee1 < employee2); //false
-            Console.WriteLine(employee1 == employee2); //true
-            Console.WriteLine(employee1 != employee2); //false
-
+            Console.WriteLine();
+            Console.WriteLine("When employee2 earns less than employee1.");
             employee2 -= 1000;
             employee1.ShowInfo();
             employee2.ShowInfo();
-
-            Console.WriteLine("When employee2 earns less than employee1.");
-            Console.WriteLine(employee1 > employee2); //true
-            Console.WriteLine(employee1 < employee2); //false
-            Console.WriteLine(employee1 == employee2); //false
-            Console.WriteLine(employee1 != employee2); //true
+            Console.WriteLine($"employee1 > employee2: {employee1 > employee2}");
+            Console.WriteLine($"employee1 < employee2: {employee1 < employee2}");
+            Console.WriteLine($"employee1 == employee2: {employee1 == employee2}");
+            Console.WriteLine($"employee1 != employee2: {employee1 != employee2}");
         }
     }
 }
