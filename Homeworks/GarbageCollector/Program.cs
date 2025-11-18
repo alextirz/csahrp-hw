f4b507c90bd84ef78f6ed8d0ee75b74c059a22a9
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-11-18 19:10:40 +0100

    test the play

--- Homeworks/GarbageCollector/Program.cs
+++ Homeworks/GarbageCollector/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GarbageCollector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test("Lisova Pisnia", "Lesya Ukrainka", "Fantasy drama", 1911);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine();
+            Console.WriteLine("NEXT");
+            Test("Konotopska Vidma", "Hryhorii Kvitka-Osnovianenko", "Satirical novella/drama", 1833);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine();
+            Console.WriteLine("ZANAVES.");
+        }
+
+        private static void Test(string title, string author, string genre, int year)
+        {
+            var play = new Play(title, author, genre, year);
+
+            Console.WriteLine(play.ShortDescription());
+            Console.WriteLine(play.ShowFullInfo());
+
+            play.Perform();
+        }
+    }
+}
