9f5e6ff0791b1a4677b4d08d1883900b314c7766
Oleksandra Bardanova <tirz_hillelcsharp@outlook.com>
2025-12-06 18:36:23 +0100

    Fix the logic and add explanations

--- Homeworks/Threading/Program.cs
+++ Homeworks/Threading/Program.cs
@@ -3,37 +3,19 @@
     static Semaphore customers = new Semaphore(0, int.MaxValue);  // How many clients are waiting
     static Semaphore barberReady = new Semaphore(0, int.MaxValue); // Barber is ready
     static Semaphore _freeChairs = new Semaphore(1, 1); // Sync for free chairs
-    static int freeChairs = 3; // Number of free seats in the waiting room
+    static int freeChairs = 2; // Number of free seats in the waiting room
+    static int numberOfcustomers = 6;
 
     static void Main()
     {
         Thread barber = new Thread(BarberWork);
         barber.Start();
 
-        for (int i = 1; i <= 2; i++)
+        for (int i = 1; i <= numberOfcustomers; i++)
         {
             Thread customer = new Thread(CustomerArrives);
             customer.Start(i);
-            Thread.Sleep(1000);
-        }
-    }
-
-    static void CustomerArrives(object id)
-    {
-        Console.WriteLine($"Client {id} tries to enter.");
-        _freeChairs.WaitOne();
-        if (freeChairs > 0)
-        {
-            freeChairs--;
-            Console.WriteLine($"Client {id} is waiting. Free chairs left: {freeChairs}");
-            customers.Release(); // Notify the barber
-            _freeChairs.Release();
-            barberReady.WaitOne(); // Wait until the barber invites
-        }
-        else
-        {
-            Console.WriteLine($"Client {id} leaves without haircut.");
-            _freeChairs.Release();
+            Thread.Sleep(100);
         }
     }
 
@@ -41,23 +23,38 @@
     {
         while (true)
         {
+            Console.WriteLine("Barber is waiting for the client.");
             customers.WaitOne(); // Sleep until a client arrives
             _freeChairs.WaitOne();
-
-            freeChairs++; // Take a client from waiting room
-            barberReady.Release(); // Barber is ready to get 
+            freeChairs++;        
             _freeChairs.Release();
+            Console.WriteLine("Barber is working");
+            Thread.Sleep(500);
+            barberReady.Release();
+            Console.WriteLine("Barber is done and ready for the next.");
+            Console.WriteLine();
         }
     }
 
-    static void DoHaircut()
+    static void CustomerArrives(object id)
     {
-        Console.WriteLine("Barber is cutting hair...");
-        Thread.Sleep(1000);
-    }
+        Console.WriteLine($"Client {id} is trying to enter.");
+        _freeChairs.WaitOne();
+        if (freeChairs > 0)
+        {
+            freeChairs--;
+             Console.WriteLine($"Client {id} is waiting in the chair. Free chairs left: {freeChairs}");
+            _freeChairs.Release();
+            customers.Release(); // Notify the barber
+            Console.WriteLine($"Client {id} Notified the barber he is here!");
+            barberReady.WaitOne(); // Wait until the barber invites
 
-    static void CutHair(object id)
-    {
-        Console.WriteLine($"Client {id} is getting a haircut.");
+            Console.WriteLine($"Client {id} leaves WITH haircut.");
+        }
+        else
+        {
+            Console.WriteLine($"Client {id} leaves WITHOUT haircut.");
+            _freeChairs.Release();
+        }
     }
 }
